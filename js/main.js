13decb29c28e9809a83687a0db5fe28b367b4039
emmazarate59 <emmazarate59@gmail.com>
2023-01-23 17:40:25 -0300

    se arregla pre entrega 2

--- js/main.js
+++ js/main.js
@@ -1,16 +1,34 @@
-const dolar = 345;
-function precioDolar(pesoArgentino) {
-    return pesoArgentino + (pesoArgentino * dolar)
-
-}
-
-let pesoArgentino;
-do {
-    pesoArgentinoIngresado = prompt("Ingreso cuantos dolares necesita, para finalizar ESC");
-    const pesoArgentino = parseFloat(pesoArgentinoIngresado)
-    if (pesoArgentino > 0) {
-        alert("Pesos necesarios: " + precioDolar(pesoArgentino));
-
+const palabraAdivinanza = ["Calle", "Una Calle"];
+let adivinoLaPalabra = false;
+let palabraIngresada;
+const intentos = [];
+class Intento {
+    constructor(entrada) {
+        this.palabra = entrada.palabra;
+        this.fecha = entrada.fecha;
+        this.palabraAdivinanza = entrada.palabraAdivinanza;
     }
 
-} while (pesoArgentinoIngresado !== "ESC");
+    esPalabraCorrecta(palabraIngresada) {
+        return this.palabraAdivinanza.some((palabraAdivinanza) => palabraAdivinanza.toLocaleLowerCase() === palabraIngresada.toLowerCase())
+    }
+}
+
+do {
+    palabraIngresada = prompt ("Descubra la palabra de la adivinanza: Todos pasan preguntando por mí, pero yo ni paso ni pregunto por nadie. o ESC para finalizar");
+    const intento = new Intento({ palabra: palabraIngresada, fecha: new Date (), palabraAdivinanza: palabraAdivinanza});
+
+    if (palabraIngresada !== "ESC") {
+        adivinoLaPalabra = intento.esPalabraCorrecta(palabraIngresada);
+        if (adivinoLaPalabra) {
+            alert ("adivinanza completada");
+        } else{
+                intentos.push(intento);
+                alert ("No adivinaste, segui intentando");
+            }
+        
+    }
+} while (!adivinoLaPalabra && palabraIngresada !== "ESC");
+
+alert(intentos.map
+    ((intento) => `Se ingreso la palabra "${intento.palabra}" a las ${intento.fecha.toLocaleTimeString()}`).join('\n'));
